abf19868d96b7105b2b3d25ac4d6caf678fef032
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-03-20 03:50:55 -0400

    Added a Layout for the left panel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-function getHeaderHeight() {
-    const headerHeightWithMargin = document.querySelector('.header').getBoundingClientRect().height + parseInt(window.getComputedStyle(document.querySelector('.header')).marginTop) + parseInt(window.getComputedStyle(document.querySelector('.header')).marginBottom);
-    return headerHeightWithMargin;
-}
-
 document.querySelector('.theme').addEventListener('click', function() {
     this.classList.toggle('theme-on');
 });
