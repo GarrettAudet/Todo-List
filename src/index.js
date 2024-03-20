0d08d2db5690517e68f40e7243aaa9ca0bd2acf6
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-03-20 02:43:21 -0400

    Completed the Structure of the Page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+function getHeaderHeight() {
+    const headerHeightWithMargin = document.querySelector('.header').getBoundingClientRect().height + parseInt(window.getComputedStyle(document.querySelector('.header')).marginTop) + parseInt(window.getComputedStyle(document.querySelector('.header')).marginBottom);
+    return headerHeightWithMargin;
+}
+
 document.querySelector('.theme').addEventListener('click', function() {
     this.classList.toggle('theme-on');
 });
