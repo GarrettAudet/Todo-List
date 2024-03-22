bb3c6b13f35c91a84c2c6fc2b89c732828cccbea
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-03-22 17:55:52 -0400

    Added functionality to change Display

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.querySelector('.theme').addEventListener('click', function() {
     this.classList.toggle('theme-on');
 });
+
+function initializeEventListener() {
+    document.addEventListener('DOMContentLoaded', () => {
+        const tasksTitle = document.querySelector('.rightPanel h1');
+        const allTasksButton = document.querySelector('.home-all-task');
+        const todayButton = document.querySelector('.home-today');
+        const next7DaysButton = document.querySelector('.home-week');
+        const importantButton = document.querySelector('.home-important');
+    
+        allTasksButton.addEventListener('click', () => {
+            tasksTitle.textContent = 'All Tasks';
+        });
+    
+        todayButton.addEventListener('click', () => {
+            tasksTitle.textContent = 'Today';
+        });
+    
+        next7DaysButton.addEventListener('click', () => {
+            tasksTitle.textContent = 'Next 7 Days';
+        });
+    
+        importantButton.addEventListener('click', () => {
+            tasksTitle.textContent = 'Important';
+        });
+    });   
+}
+
+initializeEventListener();
