9c8e63bfea3fff153729f53e3630f3484c41a7cc
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-03-22 18:28:52 -0400

    Added highlighting for tasks

--- src/index.js
+++ src/index.js
@@ -5,25 +5,28 @@ document.querySelector('.theme').addEventListener('click', function() {
 function initializeEventListener() {
     document.addEventListener('DOMContentLoaded', () => {
         const tasksTitle = document.querySelector('.rightPanel h1');
-        const allTasksButton = document.querySelector('.home-all-task');
-        const todayButton = document.querySelector('.home-today');
-        const next7DaysButton = document.querySelector('.home-week');
-        const importantButton = document.querySelector('.home-important');
-    
-        allTasksButton.addEventListener('click', () => {
-            tasksTitle.textContent = 'All Tasks';
-        });
-    
-        todayButton.addEventListener('click', () => {
-            tasksTitle.textContent = 'Today';
-        });
-    
-        next7DaysButton.addEventListener('click', () => {
-            tasksTitle.textContent = 'Next 7 Days';
-        });
-    
-        importantButton.addEventListener('click', () => {
-            tasksTitle.textContent = 'Important';
+        const buttons = [
+            { element: document.querySelector('.home-all-task'), title: 'All Tasks' },
+            { element: document.querySelector('.home-today'), title: 'Today' },
+            { element: document.querySelector('.home-week'), title: 'Next 7 Days' },
+            { element: document.querySelector('.home-important'), title: 'Important' }
+        ];
+
+        // Function to update the title and highlight the active button
+        const updateTitleAndHighlight = (title, activeButton) => {
+            tasksTitle.textContent = title;
+            buttons.forEach(button => {
+                if (button.element === activeButton) {
+                    button.element.classList.add('highlight');
+                } else {
+                    button.element.classList.remove('highlight');
+                }
+            });
+        };
+
+        // Add click event listeners to buttons
+        buttons.forEach(button => {
+            button.element.addEventListener('click', () => updateTitleAndHighlight(button.title, button.element));
         });
     });   
 }
